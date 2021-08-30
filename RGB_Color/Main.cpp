29b3f0e3c49df843fcf205f1757382e6ca103a34
Wilson Huang <window012@hotmail.com>
2021-08-30 23:28:41 +0800

    alignment code

--- RGB_Color/Main.cpp
+++ RGB_Color/Main.cpp
@@ -11,7 +11,7 @@ int main()
 	color = (r << 16) | (g << 8) | b;
 	std::cout << "color: " << std::bitset<24>(color) << std::endl;
 	std::cout << "r:     " << std::bitset<24>(color >> 16) << std::endl;
-	std::cout << "g:     " << (std::bitset<24>((color) & 0x00FF00) >> 8) << std::endl;
-	std::cout << "b:     " << (std::bitset<24>((color) & 0x0000FF)) << std::endl;
+	std::cout << "g:     " << (std::bitset<24>(color & 0x00FF00) >> 8) << std::endl;
+	std::cout << "b:     " <<  std::bitset<24>(color & 0x0000FF) << std::endl;
 	return 0;
 }
