18217a3f5a80d07ff88933faf306a87b97ae906b
Wilson Huang <window012@hotmail.com>
2021-09-01 10:07:49 +0800

    32-bit color

--- RGB_Color/Main.cpp
+++ RGB_Color/Main.cpp
@@ -6,12 +6,14 @@ int main()
 	unsigned int color = 0u;
 	unsigned char r = 255;
 	unsigned char g = 64;
-	unsigned char b = 1;
+	unsigned char b = 67;
+	unsigned char a = 20;
 
-	color = (r << 16) | (g << 8) | b;
-	std::cout << "color: " << std::bitset<24>(color) << std::endl;
-	std::cout << "r:     " << std::bitset<24>(color >> 16) << std::endl;
-	std::cout << "g:     " << (std::bitset<24>(color & 0x00FF00) >> 8) << std::endl;
-	std::cout << "b:     " <<  std::bitset<24>(color & 0x0000FF) << std::endl;
+	color = (r << 24) | (g << 16) | (b << 8) | a;
+	std::cout << "color: " << std::bitset<32>(color) << std::endl;
+	std::cout << "r:     " << std::bitset<32>(color >> 24) << std::endl;
+	std::cout << "g:     " << (std::bitset<32>(color & 0x00FF0000) >> 16) << std::endl;
+	std::cout << "b:     " << (std::bitset<32>(color & 0x0000FF00) >> 8) << std::endl;
+	std::cout << "a:     " << std::bitset<32>(color & 0x000000FF) << std::endl;
 	return 0;
 }
